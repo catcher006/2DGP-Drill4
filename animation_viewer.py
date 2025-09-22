69b054030f2c4e799adcf60c8c5b64c9cdd9c4e1
catcher006 <seungho217@tukorea.ac.kr>
2025-09-22 14:31:51 +0900

    뛰기 4번 자세 이동

--- animation_viewer.py
+++ animation_viewer.py
@@ -42,9 +42,16 @@ def move_3():
         update_canvas()
         delay(0.1)
 
+# 뛰기 자세 이동
 def move_4():
-    pass
-
+    frame = 0
+    for x in range(0, 800, 10):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 80 + 5, 282, 80, 94, x, 170, 240, 282)
+        frame = (frame + 1) % 6
+        update_canvas()
+        delay(0.05)
 
 while True:
     #move_1()
