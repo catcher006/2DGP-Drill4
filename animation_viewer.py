76d5df529c72121e5100a6e36610ffc11899518d
catcher006 <seungho217@tukorea.ac.kr>
2025-09-22 12:38:30 +0900

    기초 크기 조정및 확인

--- animation_viewer.py
+++ animation_viewer.py
@@ -7,7 +7,9 @@ character = load_image('hero_spritesheet.png')
 
 for x in range (0,800,5):
     clear_canvas()
-
+    grass.draw(400, 30)
+    character.clip_draw(0, 0, 94, 80, x, 120, 282, 240)
     update_canvas()
+    delay(0.01)
 
 close_canvas()
