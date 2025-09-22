53a7f4e9d74fa22f73425c9bc28f89673b28fa97
catcher006 <seungho217@tukorea.ac.kr>
2025-09-22 12:40:44 +0900

    경계 자세 이동 구현

--- animation_viewer.py
+++ animation_viewer.py
@@ -5,11 +5,13 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('hero_spritesheet.png')
 
+frame = 0
 for x in range (0,800,5):
     clear_canvas()
     grass.draw(400, 30)
-    character.clip_draw(0, 0, 94, 80, x, 120, 282, 240)
+    character.clip_draw(frame * 94, 0, 94, 80, x, 120, 282, 240)
+    frame = (frame + 1) % 2
     update_canvas()
-    delay(0.01)
+    delay(0.5)
 
 close_canvas()
