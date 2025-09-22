6028eb6263d0b7d88911636a78526d12ab683776
catcher006 <seungho217@tukorea.ac.kr>
2025-09-22 12:44:44 +0900

    자세 1번 구현

--- animation_viewer.py
+++ animation_viewer.py
@@ -5,18 +5,16 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('hero_spritesheet.png')
 
-frame = 0
-for x in range (0,800,5):
-    clear_canvas()
-    grass.draw(400, 30)
-    character.clip_draw(frame * 94, 0, 94, 80, x, 120, 282, 240)
-    frame = (frame + 1) % 2
-    update_canvas()
-    delay(0.5)
-
 
 def move_1():
-    pass
+    frame = 0
+    for x in range(0, 800, 5):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 94, 0, 94, 80, x, 120, 282, 240)
+        frame = (frame + 1) % 2
+        update_canvas()
+        delay(0.5)
 
 
 def move_2():
