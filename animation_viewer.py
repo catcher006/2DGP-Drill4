c183acb529cc8bf5356e8b7be3cd2445f2a3f0a4
catcher006 <seungho217@tukorea.ac.kr>
2025-09-22 12:33:17 +0900

    루프 구현

--- animation_viewer.py
+++ animation_viewer.py
@@ -5,4 +5,9 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('hero_spritesheet.png')
 
+for x in range (0,800,5):
+    clear_canvas()
+
+    update_canvas()
+
 close_canvas()
